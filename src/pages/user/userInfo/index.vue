acdc501792002fc794590992a9c904425eec59bf
JIANGHENG\Administrator <jianghengamethyst@163.com>
2018-09-21 23:32:53 +0800

    userCenter

---
+++ src/pages/user/userInfo/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="box">
+        个人信息
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "@/sass/common.scss";
+</style>
---
+++ src/pages/user/userOrder/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="box">
+        个人订单
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "@/sass/common.scss";
+</style>
