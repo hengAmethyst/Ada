3500c0e3dd66b61b460aefb09c9384f1c0fb29d6
zZengWork <1060404440@qq.com>
2018-09-25 15:04:09 +0800

    v1.0002

---
+++ src/pages/restaurant/search/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="">
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+    }
+  },
+
+  components: {
+  },
+
+  methods: {
+  },
+
+  created () {
+    // 调用应用实例的方法获取全局数据
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "@/sass/common.scss";
+</style>
