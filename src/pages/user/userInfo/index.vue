de3a3d8fef84a74ab193c10ee7beba47989a5cfd
JIANGHENG\Administrator <jianghengamethyst@163.com>
2018-09-25 23:59:53 +0800

    user

--- src/pages/user/userInfo/index.vue
+++ src/pages/user/userInfo/index.vue
@@ -1,15 +1,64 @@
 <template>
     <div class="box">
-        个人信息
+        <div class="section-1">
+            <span>AMETHYST</span>
+            <div class="img"></div>
+        </div>
+        <div class="section-2" v-for="(item,index) in navList" :key="index">
+            <span>{{item}}</span>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data(){
+        return{
+            navList:['客服电话','关于Ada','退出登录']
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
   @import "@/sass/common.scss";
+  .box{
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      height: 100%;
+      background: #fff;
+      .section-1{
+          margin: 0 auto;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          width: 670rpx;
+          height: 251rpx;
+          border-bottom: 1rpx solid #E9E9E9;
+          span{
+              font-size: 60rpx;
+              color: #010101;
+          }
+          .img{
+              width: 130rpx;
+              height: 130rpx;
+              background: pink;
+              border-radius: 100%;
+          }
+      }
+      .section-2{
+            margin: 0 auto;
+            display: flex;
+            align-items: center;
+            width: 670rpx;
+            height: 133rpx;
+            border-bottom: 1rpx solid #E9E9E9;
+            span{
+                margin-left: 20rpx;
+                font-size: 38rpx;
+                color: #666;
+            }
+        }
+  }
 </style>
--- src/pages/user/userOrder/index.vue
+++ src/pages/user/userOrder/index.vue
@@ -1,15 +1,62 @@
 <template>
     <div class="box">
-        个人订单
+        <div class="section-1">
+            <div class="section-1-1">
+                <span>AMETHYST</span>
+                <span>查看并编辑个人资料</span>
+            </div>
+            <div class="section-1-2">
+
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data(){
+        return {
+
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
   @import "@/sass/common.scss";
+  .box{
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      height: 100%;
+      background: #fff;
+      .section-1{
+          margin: 0 auto;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          width: 670rpx;
+          height: 250rpx;
+          .section-1-1{
+              display: flex;
+              flex-direction: column;
+              width: 300rpx;
+              height: 126rpx;
+              span:nth-child(1){
+                  font-size: 60rpx;
+                  color: #1A1A1A;
+              }
+              span:nth-child(2){
+                  font-size: 30rpx;
+                  color: #999;
+              }
+          }
+          .section-1-2{
+              width: 130rpx;
+              height: 130rpx;
+              background: purple;
+              border-radius: 100%;
+          }
+      }
+  }
 </style>
