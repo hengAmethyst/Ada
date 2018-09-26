ef462bed182ae62ae8edd09af6f551c3b5bd72f4
JIANGHENG\Administrator <jianghengamethyst@163.com>
2018-09-27 00:26:13 +0800

    userOrder

---
+++ src/components/user/orderList.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="order-list">
+        <div class="section-1"></div>
+        <div class="section-2">
+            <div class="section-2-1">蜀国飘香</div>
+            <div class="section-2-2">
+                <span>川味火锅</span>
+                <span>5人</span>
+                <span>待支付</span>
+            </div>
+        </div>
+        <div class="section-3">
+            <span>今天</span>
+            <span>18:00</span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped lang="scss">
+    .order-list{
+        margin-top: 30rpx;
+        display: flex;
+        align-items: center;
+        width: 100%;
+        height: 182rpx;
+        background: #fff;
+        border-top: 1rpx solid #E9E9E9;
+        box-shadow:0px 8rpx 10rpx 0px rgba(129,140,166,0.06),0px 1rpx 0px 0px rgba(0,0,0,0.05);
+        .section-1{
+            margin-left: 40rpx;
+            width: 160rpx;
+            height: 100rpx;
+            background: red;
+        }
+        .section-2{
+            margin-left: 30rpx;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            width: 400rpx;
+            height: 100rpx;
+            .section-2-1{
+                width: 100%;
+                font-size: 32rpx;
+                color: #010101;
+                font-weight: bold;
+            }
+            .section-2-2{
+                display: flex;
+                width: 100%;
+                font-size: 28rpx;
+                color: #999999;
+                span:nth-child(2){
+                    margin-left: 27rpx;
+                }
+                span:nth-child(3){
+                    margin-left: 33rpx;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    width: 90rpx;
+                    height: 44rpx;
+                    font-size: 24rpx;
+                    color: #fff;
+                    background: #E10B22;
+                }
+            }
+        }
+        .section-3{
+            margin-right: 40rpx;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            width: 90rpx;
+            height: 100rpx;
+            span{
+                font-size: 30rpx;
+                color: #010101;
+            }
+        }
+    }
+</style>
+
+
--- src/pages/user/userOrder/index.vue
+++ src/pages/user/userOrder/index.vue
@@ -6,17 +6,37 @@
                 <span>查看并编辑个人资料</span>
             </div>
             <div class="section-1-2">
-
             </div>
         </div>
+        <div class="section-2">
+            <div class="section-2-1" :class="navIndex==0?'active-nav':''" @click="changeNav(0)">
+                即将前往
+            </div>
+            <div class="section-2-2" :class="navIndex==1?'active-nav':''"  @click="changeNav(1)">
+                历史订单
+            </div>
+        </div>
+        <div class="section-3" v-if="navIndex==0">
+            <OrderList></OrderList>
+        </div>
+        
     </div>
 </template>
 
 <script>
+import OrderList from '@/components/user/orderList'
 export default {
     data(){
         return {
-
+            navIndex: 0,// 选中的nav
+        }
+    },
+    components:{
+        OrderList
+    },
+    methods:{
+        changeNav(e){
+            this.navIndex = e
         }
     }
 }
@@ -45,17 +65,42 @@ export default {
               span:nth-child(1){
                   font-size: 60rpx;
                   color: #1A1A1A;
+                  font-weight: bold;
               }
               span:nth-child(2){
                   font-size: 30rpx;
-                  color: #999;
+                  color: #999; 
+                  font-weight: bold;
               }
           }
           .section-1-2{
               width: 130rpx;
               height: 130rpx;
-              background: purple;
+              background: red;
               border-radius: 100%;
+          }
+      }
+      .section-2{
+          display: flex;
+          width: 100%;
+          border-bottom: 1rpx solid #E9E9E9;
+          .section-2-1{
+              margin-left: 40rpx;
+              padding-bottom: 12rpx;
+              font-size: 28rpx;
+              color: #999999;
+              font-weight: bold;
+          }
+          .section-2-2{
+              margin-left: 60rpx;
+              padding-bottom: 12rpx;
+              font-size: 28rpx;
+              color: #999999;
+              font-weight: bold;
+          }
+          .active-nav{
+              color: #E10B22;
+              border-bottom: 4rpx solid #E10B22;
           }
       }
   }
