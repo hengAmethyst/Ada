bad921408ddd0205a02592a0dd71727dd8f0ecab
JIANGHENG\Administrator <jianghengamethyst@163.com>
2018-09-28 00:04:12 +0800

    修改主页入口

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="home">
-    不好意思，我是假的首页
+    <div class="section-2">
+      <div class="section-2-1">
+        了解Ada
+      </div>
+      <div class="section-2-2" @click="toList">
+        ada餐厅
+      </div>
+      <div class="section-2-3">
+        附近ada
+      </div>
+    </div>
+    <div></div>
+    <div></div>
   </div>
 </template>
 
@@ -16,6 +28,11 @@ export default {
   },
 
   methods: {
+    toList(){
+      wx.navigateTo({
+        url: '/pages/restaurant/list/main'
+      })
+    }
   },
 
   created () {
@@ -26,4 +43,39 @@ export default {
 
 <style lang="scss" scoped>
   @import "@/sass/common.scss";
+  .home{
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    .section-2{
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      .section-2-1{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 300rpx;
+        background: yellow;
+      }
+      .section-2-2{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 300rpx;
+        background: blue;
+      }
+      .section-2-3{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 300rpx;
+        background: pink;
+      }
+    }
+  }
 </style>
